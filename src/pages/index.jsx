828f48d804aa62f6c00470751a0cbdda1424b279
HyungJinHan <han1210_36@naver.com>
2023-02-07 22:16:34 +0900

    서버 사이드에서 요청한 데이터를 미리 불러오고 HTML로 뿌려주는 작업 진행

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,14 @@
 import Seo from 'components/Seo';
 import React, { useEffect, useState } from 'react';
 
-function Index(props) {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(
-        `/api/movies`
-      )).json();
-      // config 리다이렉트 설정을 통한 API_KEY 숨기기
-
-      setMovies(results);
-    })();
-  }, [])
-
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title='Home' />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(
+      {results?.map(
         (movie) => (
           <div className='movie' key={movie.id}>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt='none' />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt='none' />
             <h4>{movie.original_title}</h4>
           </div>
         )
@@ -50,6 +36,16 @@ function Index(props) {
       `}</style>
     </div>
   );
-}
+};
 
-export default Index;
+// 서버 사이드에서 데이터를 요청해서 HTML에 저장하는 식으로 작동하여 로딩을 없애주는 작업
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  // config 리다이렉트 설정을 통한 API_KEY 숨기기
+
+  return {
+    props: {
+      results
+    }
+  }
+}
