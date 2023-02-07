4b3886984025b034652d4f5354780431b8cd8745
HyungJinHan <han1210_36@naver.com>
2023-02-07 15:51:21 +0900

    버전 차이로 인해 a 태그 삭제 및 Link에 직접적인 클래스, 스타일 적용 가능 엄격모드 false로 수정

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,30 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 function NavBar(props) {
+  const router = useRouter();
+  console.log(router)
+
   return (
     <nav>
-      <Link href='/'>Home</Link>
-      <Link href='/about'>About</Link>
-    </nav>
+      <Link
+        style={{ textDecoration: `none` }}
+        className='home'
+        href='/'
+      >
+        Home
+      </Link>
+
+      &nbsp;&nbsp;
+
+      <Link
+        style={{ textDecoration: `none` }}
+        className='about'
+        href='/about'>
+        About
+      </Link>
+    </nav >
   );
 }
 
